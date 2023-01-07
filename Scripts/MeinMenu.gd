4832264208b54a2fdbc31e6956126e702dda6ac9
Yoshi <dleonavichus@mail.ru>
2023-01-07 16:39:20 +0300

    add banner

--- Scripts/MeinMenu.gd
+++ Scripts/MeinMenu.gd
@@ -2,15 +2,18 @@ extends Control
 
 func _ready():
 	Glob.load_game()
+	$YandexAds.load_banner()
 
 func _on_Exit_pressed():
 	get_tree().quit()
 
 func _on_StartGame_pressed():
 	get_tree().change_scene("res://Level/Menu/Levels.tscn")
+	$YandexAds.hide_banner()
 
 func _on_Store_pressed():
 	get_tree().change_scene("res://Level/Menu/Store.tscn")
+	$YandexAds.hide_banner()
 
 func _on_close_pressed():
 	$Internet.visible = false
@@ -28,6 +31,8 @@ func _on_vk_pressed():
 func _on_discord_pressed():
 	OS.shell_open('https://discord.com/invite/JMMa9aHh8f')
 
-
 func _on_telegram_pressed():
 	OS.shell_open('https://t.me/+bXvLJdzO2pZiZGVi')
+
+func _on_YandexAds_banner_loaded():
+	$YandexAds.show_banner()
